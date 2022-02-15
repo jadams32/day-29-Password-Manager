ce503d063503020962b60a7b17190c51babcc8f9
John Adams Jr <john.c.adams32@gmail.com>
2022-02-15 10:45:23 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+from tkinter import *
+
+# ---------------------------- PASSWORD GENERATOR ------------------------------- #
+
+# ---------------------------- SAVE PASSWORD ------------------------------- #
+
+# ---------------------------- UI SETUP ------------------------------- #
+window = Tk()
+window.title("Password Manager")
+window.config(padx=200, pady=200)
+
+canvas = Canvas(width=200, height=200)
+canvas.grid(column=2, row=0)
+
+
+
+window.mainloop()
