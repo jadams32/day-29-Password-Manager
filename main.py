a4dffc9c550d47147281cdcd4b2784fa90dda0af
John Adams Jr <john.c.adams32@gmail.com>
2022-02-15 14:23:38 -0700

    Final with comments.

--- main.py
+++ main.py
@@ -55,6 +55,7 @@ window = Tk()
 window.title("Password Manager")
 window.config(padx=50, pady=50)
 
+# Lock Logo Display
 canvas = Canvas(width=200, height=200)
 lock_logo = PhotoImage(file="logo.png")
 canvas.create_image(100, 100, image=lock_logo)
