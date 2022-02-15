d0bfad0ff139398f5b064a948ce2c6463af8bc09
John Adams Jr <john.c.adams32@gmail.com>
2022-02-15 15:38:02 -0700

    json filenotfound error solved

--- main.py
+++ main.py
@@ -46,12 +46,16 @@ def save():
                                                                            f"Email: {email_data}\n "
                                                                            f"Password: {password_data}")
         if valid:
-            with open('data.json', 'r') as file:
-                data = json.load(file)
+            try:
+                with open('data.json', 'r') as file:
+                    data = json.load(file)
+            except FileNotFoundError:
+                with open('data.json', 'w') as file:
+                    json.dump(new_data, file, indent=4)
+            else:
                 data.update(new_data)
-
-            with open('data.json', 'w') as file:
-                json.dump(data, file, indent=4)
+                with open('data.json', 'w') as file:
+                    json.dump(data, file, indent=4)
 
             delete_all()
             messagebox.showinfo(title="Password Manager", message='Password Saved!')
