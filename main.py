22743fbe42c0952f6e016d04cb85bdd142c6b24b
John Adams Jr <john.c.adams32@gmail.com>
2022-02-15 12:00:43 -0700

    final layout finished

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from tkinter import *
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title("Password Manager")
-window.config(padx=20, pady=20)
+window.config(padx=50, pady=50)
 
 canvas = Canvas(width=200, height=200)
 lock_logo = PhotoImage(file="logo.png")
@@ -16,23 +16,23 @@ canvas.grid(column=1, row=0)
 
 website_label = Label(text="Website:")
 website_label.grid(column=0, row=1)
-website_input = Entry()
+website_input = Entry(width=35)
 website_input.grid(column=1, row=1, columnspan=2)
 
-email_label = Label(text="Email / Username:")
+email_label = Label(text="Email/Username:")
 email_label.grid(column=0, row=2)
-email_input = Entry()
+email_input = Entry(width=35)
 email_input.grid(column=1, row=2, columnspan=2)
 
 password_label = Label(text="Password:")
 password_label.grid(column=0, row=3)
-password_input = Entry()
+password_input = Entry(width=18)
 password_input.grid(column=1, row=3)
 
-generate_pass_button = Button(text="Generate Password")
+generate_pass_button = Button(text="Generate Password", width=13)
 generate_pass_button.grid(column=2, row=3)
 
-add_button = Button(text="Add")
+add_button = Button(text="Add", width=35)
 add_button.grid(column=1, row=4, columnspan=2)
 
 
