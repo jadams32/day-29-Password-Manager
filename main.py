5082663eabc68cf4537042fb93415618cfeb690e
John Adams Jr <john.c.adams32@gmail.com>
2022-02-15 11:36:08 -0700

    basic interface done

--- main.py
+++ main.py
@@ -12,7 +12,29 @@ window.config(padx=20, pady=20)
 canvas = Canvas(width=200, height=200)
 lock_logo = PhotoImage(file="logo.png")
 canvas.create_image(100, 100, image=lock_logo)
-canvas.grid(column=2, row=0)
+canvas.grid(column=1, row=0)
+
+website_label = Label(text="Website:")
+website_label.grid(column=0, row=1)
+website_input = Entry()
+website_input.grid(column=1, row=1, columnspan=2)
+
+email_label = Label(text="Email / Username:")
+email_label.grid(column=0, row=2)
+email_input = Entry()
+email_input.grid(column=1, row=2, columnspan=2)
+
+password_label = Label(text="Password:")
+password_label.grid(column=0, row=3)
+password_input = Entry()
+password_input.grid(column=1, row=3)
+
+generate_pass_button = Button(text="Generate Password")
+generate_pass_button.grid(column=2, row=3)
+
+add_button = Button(text="Add")
+add_button.grid(column=1, row=4, columnspan=2)
+
 
 
 
